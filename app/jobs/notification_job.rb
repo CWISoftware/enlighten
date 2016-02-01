6cb410e41acc19d578d256dea2b2501d76305a50
Rafael Eyng <rafaeleyng@gmail.com>
2016-02-01 18:59:29 -0200

    Refactor notification

--- app/jobs/notification_job.rb
+++ app/jobs/notification_job.rb
@@ -2,10 +2,10 @@ class NotificationJob < ActiveJob::Base
   queue_as :default
 
   def perform(*args)
-    entity = args.first
-    followers = entity.followers.joins(:person).where(people: { notification: true })
+    followed = args.first
+    followers = followed.followers.joins(:person).where(people: { notification: true })
     followers.each do |follower|
-      NotificationMailer.notification_email(entity, follower).deliver_later
+      NotificationMailer.notification_email(followed, follower).deliver_later
     end
   end
 end
--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,7 +1,7 @@
 class NotificationMailer < ApplicationMailer
-  def notification_email(entity, follower)
-    @entity = entity
-    @person = follower.person
-    mail(to: follower.email, subject: "#{entity.name} has some updates")
+  def notification_email(followed, follower)
+    @followed = followed
+    @follower_person = follower.person
+    mail(to: follower.email, subject: "#{followed.name} has some updates")
   end
 end
